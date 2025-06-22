9df97ede3a778c710109c22563eeb61e9de5907e
Malte Ruff <112979235+RuffMalte@users.noreply.github.com>
2025-06-22 18:18:57 +0200

    Initial Commit

---
+++ BunGen/BunGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  BunGenApp.swift
+//  BunGen
+//
+//  Created by Malte Ruff on 22.06.25.
+//
+
+import SwiftUI
+
+@main
+struct BunGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
